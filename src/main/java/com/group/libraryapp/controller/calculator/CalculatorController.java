2783a14c066e839eef4b55ec042ac4467757bf7a
timo <masonmount323@gmail.com>
2023-05-29 15:46:24 +0900

    02. POST API(Multiply)

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,14 +1,14 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // API의 진입지점으로 만들겠다.
 public class CalculatorController {
 
     @GetMapping("/add")
-    public int addTwoNumbers(CalculatorAddRequest request){
+    public int addTwoNumbers(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
     }
 
@@ -18,4 +18,15 @@ public class CalculatorController {
 //    ) {
 //        return number1 + number2;
 //    }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        System.out.println("Result is ");
+        return request.getNumber1() * request.getNumber2();
+    }
+
+    @GetMapping("/add/rp")
+    public int addTwoNumbersByRequestParam(@RequestParam int number1, @RequestParam int number2) {
+        return number1 + number2;
+    }
 }
