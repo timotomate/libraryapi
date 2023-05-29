effd04191a4548fb2cceb938640bffc045803be9
timo <masonmount323@gmail.com>
2023-05-29 15:34:56 +0900

    fix01. query -> Body

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,17 +1,21 @@
 package com.group.libraryapp.controller.calculator;
 
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // API의 진입지점으로 만들겠다.
 public class CalculatorController {
 
     @GetMapping("/add")
-    public int addTwoNumbers(
-            @RequestParam int number1,
-            @RequestParam int number2
-    ) {
-        return number1 + number2;
+    public int addTwoNumbers(CalculatorAddRequest request){
+        return request.getNumber1() + request.getNumber2();
     }
+
+//    public int addTwoNumbers(
+//            @RequestParam int number1,
+//            @RequestParam int number2
+//    ) {
+//        return number1 + number2;
+//    }
 }
